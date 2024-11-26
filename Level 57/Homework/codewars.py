c41622aca7a8c6c57f878da060476dc0bf143c11
MGDACHI <dachidzagnidze2009@gmail.com>
2024-11-26 21:12:52 +0400

    second commit

--- Level 57/Homework/codewars.py
+++ Level 57/Homework/codewars.py
@@ -11,6 +11,22 @@ def area_or_perimeter(l, w):
 def string_to_array(s):
     return s.split() if s != '' else ['']
 
+#############################
+def string_to_array(s):
+    result = []
+    word = ''
+    for char in s:
+        if char == ' ':
+            if word:
+                result.append(word)
+                word = ''
+        else:
+            word += char
+    if word:
+        result.append(word)  # Add the last word if it exists
+    return result if s else ['']
+
+
 #! codewars task N3
 def feast(beast, dish):
     return beast[0] == dish[0] and beast[-1] == dish[-1]
