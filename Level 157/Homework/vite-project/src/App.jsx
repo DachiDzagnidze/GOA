aaaf38121967da1dda19d37844f3265ce192d4a0
DachiDzagnidze <Dachidzagnidze2009@gmail.com>
2026-01-10 19:27:49 +0400

    second commit

--- Level 157/Homework/vite-project/src/App.jsx
+++ Level 157/Homework/vite-project/src/App.jsx
@@ -4,16 +4,24 @@ import UsersList from "./components/UsersList";
 import LoadPostsOnClick from "./components/LoadPostsOnClick";
 
 export default function App() {
+  const sectionStyle = { marginBottom: "20px" }; // reusable style
+
   return (
     <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>Single Post</h1>
-      <SinglePost />
+      <div style={sectionStyle}>
+        <h1>Single Post</h1>
+        <SinglePost />
+      </div>
 
-      <h1>Users List</h1>
-      <UsersList />
+      <div style={sectionStyle}>
+        <h1>Users List</h1>
+        <UsersList />
+      </div>
 
-      <h1>Load Posts on Button Click</h1>
-      <LoadPostsOnClick />
+      <div style={sectionStyle}>
+        <h1>Load Posts on Button Click</h1>
+        <LoadPostsOnClick />
+      </div>
     </div>
   );
 }
